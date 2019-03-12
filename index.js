1772791113a7a7aea1c056d405af6f696f99c378
Ben Hancock <benjaminhancock@hotmail.co.uk>
2019-03-12 15:00:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+function  destructivelyAppendKitten() {
+kittens.push('Ralph');
+return kittens;
+}
+
+function  destructivelyPrependKitten()  {
+kittens.unshift('Bob');
+return kittens;
+}
+
+function  destructivelyRemoveLastKitten() {
+kittens.pop();
+return  kittens;
+}
+
+function  destructivelyRemoveFirstKitten()  {
+kittens.shift()
+return kittens;
+}
+//problem
+function  appendKitten()  {
+  var newKittens = [...kittens, "Broom"];
+  return newKittens;
+}
+//problem
+function  prependKitten() {
+  var newKittens = "Arnold");
+  return  newKittens;
+}
+function  removeLastKitten()  {
+  kittens.pop();
+  kittens;
+}
+
+function  removeFirstKitten() {
+
+}
